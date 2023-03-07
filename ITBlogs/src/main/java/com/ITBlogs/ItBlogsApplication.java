4a1451a06ec5e9bc779780e00106fb6a6e28a101
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-03-07 22:06:48 +0100

    init

---
+++ ITBlogs/src/main/java/com/ITBlogs/ItBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.ITBlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBlogsApplication.class, args);
+	}
+
+}
