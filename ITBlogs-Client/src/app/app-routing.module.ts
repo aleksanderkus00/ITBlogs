57a32068eb8a7092f528b9337d8564febb6b635a
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-03-19 14:29:46 +0100

    Create defaullt page

--- ITBlogs-Client/src/app/app-routing.module.ts
+++ ITBlogs-Client/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { NewsComponent } from './components/news/news.component';
+import { DiscoverComponent } from './components/discover/discover.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostsComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'discover', component: DiscoverComponent },
+  { path: 'saved', component: DiscoverComponent },
+  { path: 'settings', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ITBlogs-Client/src/app/app.module.ts
+++ ITBlogs-Client/src/app/app.module.ts
@@ -6,29 +6,48 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtModule } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NewsComponent } from './components/news/news.component';
+import { DiscoverComponent } from './components/discover/discover.component';
+import { SavedComponent } from './components/saved/saved.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
 }
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchBarComponent,
+    PostComponent,
+    PostsComponent,
+    NewsComponent,
+    DiscoverComponent,
+    SavedComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    ScrollingModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
         allowedDomains: [environment.allowedDomains],
         disallowedRoutes: [''],
       },
-    })
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
