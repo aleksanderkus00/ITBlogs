e127c84c6951219a100ce86db0655d833e6ac99c
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-03-19 11:09:58 +0100

    Add dependencies

--- ITBlogs-Client/src/app/app.module.ts
+++ ITBlogs-Client/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtModule } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: [environment.allowedDomains],
+        disallowedRoutes: [''],
+      },
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
