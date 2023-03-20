4025dc82d1f632bf73e6da6d6563e6ff1f48f36a
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-03-20 18:05:40 +0100

    Create settings section

--- ITBlogs-Client/src/app/services/user.service.ts
+++ ITBlogs-Client/src/app/services/user.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserService {
-  public user: boolean = false;
+  public user: boolean = true;
 
   constructor() {}
 }
