cef4074cbd21c6bc315a61f1b2aea4115b92ec1c
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-03-20 16:38:21 +0100

    Imitale user for now

--- ITBlogs-Client/src/app/components/navbar/navbar.component.ts
+++ ITBlogs-Client/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  constructor(public userService: UserService) {}
 }
--- ITBlogs-Client/src/app/components/search-bar/search-bar.component.ts
+++ ITBlogs-Client/src/app/components/search-bar/search-bar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+  constructor(public userService: UserService) {}
+}
