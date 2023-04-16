17c32a7419be66dbef50292c4c703b2d62c989ee
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-04-16 13:38:30 +0200

    Implement endpoint bodies

--- ITBlogs/src/main/java/com/ITBlogs/controllers/ArticleController.java
+++ ITBlogs/src/main/java/com/ITBlogs/controllers/ArticleController.java
@@ -2,35 +2,80 @@ package com.ITBlogs.controllers;
 
 import com.ITBlogs.models.Article;
 import com.ITBlogs.models.PaginatedResult;
+import com.ITBlogs.repository.ArticleRepository;
+import com.ITBlogs.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/article")
 public class ArticleController {
 
-    @PostMapping()
-    Article createArticle(@RequestBody Article article) {
+    private final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+    private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+
+    public ArticleController(ArticleRepository articleRepository, UserRepository userRepository) {
+        this.articleRepository = articleRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/{userId}")
+    Article createArticle(@RequestBody Article article, @PathVariable long userId) {
+        try {
+            var user = this.userRepository.findById(userId).get();
+            article.setUser(user);
+            article.setGeneratedDate(LocalDateTime.now());
+            return this.articleRepository.save(article);
+        } catch (Exception exception) {
+            this.logger.error(exception.getMessage());
+        }
         return null;
     }
 
     @GetMapping("/{id}")
     Article getArticleById(@PathVariable long id) {
+        try{
+            return this.articleRepository.findById(id).get();
+        } catch (NoSuchElementException exception) {
+            this.logger.warn("Cannot get user with id: " + id);
+        }
         return null;
     }
 
     @PutMapping("/{id}")
-    Article updateArticleById(@RequestBody Article article, @PathVariable long id) {
+    Article updateArticleById(@RequestBody Article updatedArticle, @PathVariable long id) {
         return null;
     }
 
     @DeleteMapping("/{id}")
     void deleteArticleById(@PathVariable long id) {
+        try{
+            var article = this.articleRepository.findById(id).get();
+            article.setDeleted(true);
+            this.articleRepository.save(article);
+        } catch (NoSuchElementException exception) {
+            this.logger.warn("Cannot delete user with id: " + id);
+        }
     }
 
-    @GetMapping("/all-articles")
-    PaginatedResult<List<Article>> getAllArticles(){
-        return null;
+    @GetMapping("/all-articles/{pageNumber}/{pageSize}")
+    //PaginatedResult<List<Article>> getAllArticles(
+    List<Article> getAllArticles(
+            @PathVariable long pageNumber,
+            @PathVariable long pageSize)
+    {
+        var data =  this.articleRepository.findAll()
+                .stream()
+                .skip(pageNumber * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+        return data;
     }
 }
--- ITBlogs/src/main/java/com/ITBlogs/controllers/UserController.java
+++ ITBlogs/src/main/java/com/ITBlogs/controllers/UserController.java
@@ -1,29 +1,59 @@
 package com.ITBlogs.controllers;
 
 import com.ITBlogs.models.User;
+import com.ITBlogs.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping()
     User createUser(@RequestBody User user) {
-        return null;
+        return this.userRepository.save(user);
     }
 
     @GetMapping("/{id}")
     User getUserById(@PathVariable long id) {
-        return null;
+        try {
+            return this.userRepository.findById(id).get();
+        } catch (NoSuchElementException exception) {
+            this.logger.warn("Cannot get user with id: " + id);
+            return null;
+        }
     }
 
     @PutMapping("/{id}")
-    User updateUserById(@RequestBody User user, @PathVariable long id) {
+    User updateUserById(@RequestBody User updatedUser, @PathVariable long id) {
+        try {
+            var user = this.userRepository.findById(id).get();
+            user.setUsername(updatedUser.getUsername());
+            user.setEmail(updatedUser.getEmail());
+            this.userRepository.save(user);
+        } catch (NoSuchElementException exception){
+            this.logger.warn("Cannot update user with id: " + id);
+        }
         return null;
     }
 
     @DeleteMapping("/{id}")
     void deleteUserById(@PathVariable long id) {
-
+        try {
+            var user = this.userRepository.findById(id).get();
+            user.setDeleted(true);
+            this.userRepository.save(user);
+        } catch (NoSuchElementException exception) {
+            this.logger.warn("Cannot get user with id: " + id);
+        }
     }
 }
