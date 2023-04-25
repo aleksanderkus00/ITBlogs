bf9a6880a9dad74f135641ee0dd60da4050e37f7
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-04-25 23:58:49 +0200

    Change articles post preview logic

--- ITBlogs-Client/src/app/components/post/post.component.ts
+++ ITBlogs-Client/src/app/components/post/post.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from 'src/app/models/article.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {}
+export class PostComponent {
+  @Input() article: Article | undefined;
+}
--- ITBlogs-Client/src/app/components/posts/posts.component.ts
+++ ITBlogs-Client/src/app/components/posts/posts.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from 'src/app/models/article.model';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {
-  posts: any[] = [1, 2, 3, 4, 5, 6, 7];
+export class PostsComponent implements OnInit {
+  articles = new Observable<Article[] | undefined>();
+
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.articles = this.articleService.getAllArticles();
+  }
 }
