3acf24b5bd170af87da9432aee65be4831761d5c
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-04-25 23:59:22 +0200

    Create create article component

--- ITBlogs-Client/src/app/components/create-article/create-article.component.ts
+++ ITBlogs-Client/src/app/components/create-article/create-article.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Article } from 'src/app/models/article.model';
 
 @Component({
   selector: 'app-create-article',
   templateUrl: './create-article.component.html',
   styleUrls: ['./create-article.component.scss'],
 })
-export class CreateArticleComponent {}
+export class CreateArticleComponent {
+  article: Article = {};
+  public Editor = ClassicEditor;
+
+  public save(): void {
+    console.log(this.article);
+  }
+}
