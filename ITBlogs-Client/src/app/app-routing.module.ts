c9e3286d2f0303f98b463c2914e884852ab66b78
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-04-26 11:58:17 +0200

    Fix wrong route

--- ITBlogs-Client/src/app/app-routing.module.ts
+++ ITBlogs-Client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { CreateArticleComponent } from './components/create-article/create-article.component';
 import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { SavedComponent } from './components/saved/saved.component';
 
 const routes: Routes = [
   {
@@ -18,7 +19,7 @@ const routes: Routes = [
   { path: 'post-details/:id', component: PostDetailsComponent },
   { path: 'news', component: NewsComponent },
   { path: 'discover', component: DiscoverComponent },
-  { path: 'saved', component: DiscoverComponent },
+  { path: 'saved', component: SavedComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'create-article', component: CreateArticleComponent },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' },
